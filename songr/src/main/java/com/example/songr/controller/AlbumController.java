a79c739d07e34f5d2bab3e830416b0ca35bfb720
haneenbader <haneenalwatan993@gmail.com>
2021-08-24 20:58:56 +0300

    solve lab12

--- songr/src/main/java/com/example/songr/controller/AlbumController.java
+++ songr/src/main/java/com/example/songr/controller/AlbumController.java
@@ -24,19 +24,26 @@ public class AlbumController {
         return "Albums.html";
     }
 
-
+//        commit it after added to DB
     // post request
-    @GetMapping("/newAlbums") // this should be a post, we are just adding it as get to run the function without using a form
+//    @GetMapping("/newAlbums") // this should be a post, we are just adding it as get to run the function without using a form
     //create + save + return
-    public RedirectView addNewAlbums() {
-        Albums albums1 = new Albums ("Baby shark" , "Pinkfong Songs " ,5 ,4020, "https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_US/daag1-CW999FTDJ35-Full-Image_GalleryBackground-en-US-1605902446047._SX1080_.png");
-        Albums albums2 = new Albums ("Wheels on the Bus " , "CoComelon " ,3 ,3255, "https://cdns-images.dzcdn.net/images/cover/f8598dd0d54c5c5ca38eaade0e7f5dbe/500x500.jpg");
-        Albums albums3 = new Albums ("Balloon Boat Race " , "CoComelon " ,3 ,3652, "https://wp-live-videos-bucket.s3.ap-south-1.amazonaws.com/UCbCmjCuTUZos6Inko4u57UQ/WRVsOCh907o.jpg");
+//    public RedirectView addNewAlbums() {
+//        Albums albums1 = new Albums ("Baby shark" , "Pinkfong Songs " ,5 ,4020, "https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_US/daag1-CW999FTDJ35-Full-Image_GalleryBackground-en-US-1605902446047._SX1080_.png");
+//        Albums albums2 = new Albums ("Wheels on the Bus " , "CoComelon " ,3 ,3255, "https://cdns-images.dzcdn.net/images/cover/f8598dd0d54c5c5ca38eaade0e7f5dbe/500x500.jpg");
+//        Albums albums3 = new Albums ("Balloon Boat Race " , "CoComelon " ,3 ,3652, "https://wp-live-videos-bucket.s3.ap-south-1.amazonaws.com/UCbCmjCuTUZos6Inko4u57UQ/WRVsOCh907o.jpg");
+//
+//        albumsRepository.save(albums1);
+//        albumsRepository.save(albums2);
+//        albumsRepository.save(albums3);
 
-        albumsRepository.save(albums1);
-        albumsRepository.save(albums2);
-        albumsRepository.save(albums3);
+//        return new RedirectView("/albums");
+//    }
+@PostMapping("/addAlbum")
+public RedirectView  addFormData (@ModelAttribute Albums album , Model model){
+    model.addAttribute("album" , album);
+    albumsRepository.save(album);
+    return new RedirectView("/albums");
+}
 
-        return new RedirectView("/albums");
-    }
 }
